8ff18cc75a96459eee0ce78536ffd649077851ed
Abhishek Mahajan <abhishekmahajan@alphaseam.com>
2025-06-24 14:57:37 +0530

    refresttoken swagger

--- src/main/java/com/hotelapi/dto/RefreshTokenRequest.java
+++ src/main/java/com/hotelapi/dto/RefreshTokenRequest.java
@@ -1,8 +1,16 @@
 package com.hotelapi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Request payload for refreshing JWT token")
 public class RefreshTokenRequest {
+
+    @Schema(
+        description = "The refresh token issued during login",
+        example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+        required = true
+    )
     private String refreshToken;
 }
