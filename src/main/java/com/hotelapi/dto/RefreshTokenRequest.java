e4866cef7464853f234c001e012841c8b0b0ea96
Abhishek Mahajan <abhishekmahajan@alphaseam.com>
2025-06-24 14:17:18 +0530

    .

--- src/main/java/com/hotelapi/dto/RefreshTokenRequest.java
+++ src/main/java/com/hotelapi/dto/RefreshTokenRequest.java
@@ -1,9 +1,7 @@
 package com.hotelapi.dto;
 
 import lombok.Data;
-import io.swagger.v3.oas.annotations.Hidden;
 
-@Hidden
 @Data
 public class RefreshTokenRequest {
     private String refreshToken;
